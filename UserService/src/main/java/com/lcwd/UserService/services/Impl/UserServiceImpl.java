08c3fbc16a286b5f96152b5366ed84c04e60b8e0
upendra singh <upendra.singh111988@gmail.com>
2024-08-21 21:46:36 +0530

    implemented rest template for communication b/t microservices

--- UserService/src/main/java/com/lcwd/UserService/services/Impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/UserService/services/Impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.lcwd.UserService.services.Impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.apache.logging.log4j.Logger;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.lcwd.UserService.UserRepo.UserRepo;
 import com.lcwd.UserService.entity.User;
@@ -18,6 +23,11 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserRepo userRepo;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private org.slf4j.Logger logger=LoggerFactory.getLogger(UserServiceImpl.class);
 
 	@Override
 	public User saveUser(User user) {
@@ -38,6 +48,12 @@ public class UserServiceImpl implements UserService {
 		User singleUser = userRepo.findById(userId)
 		.orElseThrow(()-> new ResourceNotFoundException("Resource Not found exception","userId","userId"));
 		
+		// fetch ratings of the above user from rating service
+		//http://localhost:8085/ratings/users/8e9e58f6-2e88-4afa-a914-b1478184b30c
+		ArrayList ratingsOfUsers = restTemplate.getForObject("http://localhost:8083/ratings/users/" + singleUser.getUserId(), ArrayList.class);
+		
+		logger.info("printing url object::"+ ratingsOfUsers);
+		singleUser.setRatings(ratingsOfUsers);
 		return singleUser;
 	}
 
