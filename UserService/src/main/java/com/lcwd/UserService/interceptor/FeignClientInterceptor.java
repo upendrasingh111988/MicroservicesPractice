f2986c738f4d01218e9c728c906b3d3aadd9be05
upendra singh <upendra.singh111988@gmail.com>
2024-08-26 16:02:21 +0530

    okta implemented

---
+++ UserService/src/main/java/com/lcwd/UserService/interceptor/FeignClientInterceptor.java
@@ -0,0 +1,15 @@
+package com.lcwd.UserService.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class FeignClientInterceptor implements RequestInterceptor {
+
+	@Override
+	public void apply(RequestTemplate template) {
+
+
+		
+	}
+
+}
